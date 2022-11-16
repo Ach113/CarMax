361f1f93e70c552a07f709e342284427c9579951
ach113 <archieberidze@gmail.com>
2022-11-16 14:20:30 -0800

    add 2nd api; improve stories

--- actions/actions.py
+++ actions/actions.py
@@ -4,13 +4,15 @@
 # See this guide on how to implement these action:
 # https://rasa.com/docs/rasa/custom-actions
 
+import sqlite3
+import pandas as pd
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 
-from .api import *
+from .api import find_state_api, find_car_api
 
 
 class ActionStoreLocation(Action):
@@ -19,7 +21,11 @@ class ActionStoreLocation(Action):
         return "action_store_location"
 
     def run(self, dispatcher, tracker, domain):
-        SlotSet("is_old_user", True)
+
+        with open('log.txt', 'a') as file:
+            car_model = tracker.get_slot('car_model')
+            zipcode = tracker.get_slot('zip')
+            file.write(f'User at {zipcode} - car model {car_model}')
 
 
 class ActionRunCarQuery(Action):
@@ -29,11 +35,58 @@ class ActionRunCarQuery(Action):
 
     def run(self, dispatcher, tracker, domain):
         car_model = tracker.get_slot('car_model')
-        loc = tracker.get_slot('location')
+        zipcode = tracker.get_slot('zip')
+
+        # 2nd api call
+        response = find_car_api(car_model, zipcode)
+
+        dispatcher.utter_message(text=response)
+
+
+class ActionCheckCar(Action):
+
+    def name(self) -> Text:
+        return "action_check_car"
+
+    def run(self, dispatcher, tracker, domain):
+        car_model = tracker.get_slot('car_model')
+        # connect to db
+        conn = sqlite3.connect('./rasa.db')
+        query = 'SELECT * FROM cars;'
+
+        df = pd.read_sql_query(query, conn)
+        df = df[df['brand'] == car_model]
+        if len(df) == 0:
+            response = f'{car_model} could not be found in our database. ' \
+                       'Please try again if you made a typo,' \
+                       'or type `search on web` if you want to search the car outside of our database'
+        else:
+            response = f'Found {len(df)} instances of {car_model}, ' \
+                       f'please enter you zip code so we can find a car closest to you'
+        dispatcher.utter_message(text=response)
+
+
+class ActionDisplayResult(Action):
+
+    def name(self) -> Text:
+        return "action_display_result"
+
+    def run(self, dispatcher, tracker, domain):
+        zipcode = tracker.get_slot('zip')
+        car_model = tracker.get_slot('car_model')
 
         # 1st api call
-        zip = find_zip_api(loc)
-        # 2nd api call
-        response = find_car_api(car_model, zip)
+        state = find_state_api(zipcode)
+        conn = sqlite3.connect('./rasa.db')
+        query = f'select * from cars where state = "{state}";'
 
+        df = pd.read_sql_query(query, conn)
+        df = df[df['brand'] == car_model].sort_values(by='price', ascending=True).iloc[0]
+        price, vin = df['price'], df['vin']
+        if len(df) == 0:
+            response = f'{car_model} could not be found in {state}. ' + \
+                'Type `search on web` if you want to search the car outside of our database'
+        else:
+            response = f'Found cheapest car - {car_model} at price of {price}$.\n' \
+                    f'You can check the vehicle using this vin: {vin}'
         dispatcher.utter_message(text=response)
--- actions/api.py
+++ actions/api.py
@@ -1,7 +1,27 @@
+import sqlite3
 import requests
 import numpy as np
+import pandas as pd
 from bs4 import BeautifulSoup
+from uszipcode import SearchEngine
 from geopy.geocoders import Nominatim
+
+
+def find_state_api(zipcode):
+    """ finds the state based on a zipcode using `uszipcode` api """
+    engine = SearchEngine()
+    zipcode = engine.by_zipcode(zipcode)
+
+    conn = sqlite3.connect('./rasa.db')
+    query = 'SELECT * FROM states;'
+
+    df = pd.read_sql_query(query, conn)
+    state_map = dict(zip(df['Abbr'].values, df['State'].values))
+
+    if not zipcode:
+        return 'california'
+    else:
+        return state_map[zipcode.state].lower()
 
 
 def find_zip_api(location):
@@ -15,17 +35,19 @@ def find_zip_api(location):
 
 
 def find_car_api(model_name, zip):
+    """ browses `cars.com` using search queries """
     header = ({
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0'
     })
 
     # search for a particular keyword
-    model_name_processed = '-'.join(x for x in model_name.split(' '))
-    url = f"https://www.cars.com/shopping/results/?models[]={model_name_processed}&page_size=20&sort=best_match_desc&zip={zip}"
+    # model_name_processed = '-'.join(x for x in model_name.split(' '))
+    url = f"https://www.cars.com/shopping/results/?models[]={model_name}&page_size=20&sort=best_match_desc&zip={zip}"
     html = BeautifulSoup(requests.get(url, headers=header).text, 'html.parser')
 
     # collect all the prices
     prices = [x.get_text(strip=True) for x in html.find_all('span', attrs={'class': 'primary-price'})]
+    prices = list(filter(lambda x: 'Priced' not in str(x), prices))
     if len(prices) == 0:
         return f'No car with the model {model_name} could be found'
     prices = [int(x.replace(',', '')[1:]) for x in prices]
